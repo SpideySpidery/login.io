ab9c50e7e6ffee12ef91ab14d99805fd34fb6af1
Spideyspidery <75521850+Spideyspidery@users.noreply.github.com>
2022-02-15 13:50:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+        var cursor = document.querySelector(".cursor");
+        document.addEventListener('mousemove', e=>{
+            cursor.setAttribute("style", "top:" + (e.pageY) +"px; left:" + (e.pageX)+"px");
+            
+        })  
